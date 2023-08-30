34c5b6c0659b02f56b400b17edf6683c5cf5fbfb
sofojedal <111465717+sofojedal@users.noreply.github.com>
2023-08-30 16:45:02 -0500

    Create html.py

---
+++ html.py
@@ -0,0 +1,25 @@
+import boto3
+import urllib.request
+from datetime import datetime
+
+
+def descargahtml():
+    urls = {
+        "eltiempo": "https://www.eltiempo.com/",
+        "publimetro": "https://www.publimetro.co/"
+    }
+    # Configurar cliente de S3
+    s3_client = boto3.client("s3")
+    # Obtener fecha actual
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    for newspaper, url in urls.items():
+        response = urllib.request.urlopen(url)
+        content = response.read()
+        # Definir la ruta en S3
+        s3_path = f"news/raw/{newspaper}-{current_date}.html"
+        # Subir contenido a S3
+        s3_client.put_object(Bucket="bucketparcial1bd1",
+                             Key=s3_path,
+                             Body=content)
+        print(f"Contenido de {newspaper} guardado en S3: {s3_path}")
+    return "hola"
